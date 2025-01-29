2a6cffc5a87ca27d186ba9261500972f1fec178c
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-01-29 14:28:01 +0200

    add explanation

--- packages/@interaxyz/mobile/src/public/hooks/useWallet.ts
+++ packages/@interaxyz/mobile/src/public/hooks/useWallet.ts
@@ -1,3 +1,17 @@
+/**
+ * With the current structure of the @interaxyz/mobile package there is a requirement to initialize
+ * missingGlobals.ts before everything else. It is imported in the src/public/createApp.ts.
+ * For this reason we cannot use top level imports using absolute internal paths like `src/redux/hooks`
+ * as it can cause those resolved paths to be included in the runtime before the globals file.
+ * The same issue happens if we try to use absolute imports like '@interaxyz/mobile/src/redux/hooks'.
+ * 
+ * Until we resolve this issue, the approach to include any code from src folder should be as following:
+ *   - top level imports of external modules are allowed (e.g. 'react')
+ *   - internal modules should be included with an inline require
+ *   - "import type" can be used to persist type-safety as it doesn't include types in the runtime
+ *   - when using "import type" - use relative paths (e.g. ../../redux/hooks)
+ */
+
 import { useMemo } from 'react'
 import type { UseSelector } from '../../redux/hooks'
 import type { SortedTokensWithBalanceOrShowZeroBalanceSelector } from '../../tokens/selectors'
