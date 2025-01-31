a10f7d0e063f6805a4c56f781fc26f7928542d8f
Jean Regisser <jean.regisser@gmail.com>
2025-01-31 11:32:00 +0100

    Update comment

--- apps/example/plugins/withConditionalDetox.js
+++ apps/example/plugins/withConditionalDetox.js
@@ -7,6 +7,9 @@ const { withPlugins } = require('@expo/config-plugins')
  */
 module.exports = (config, properties = {}) => {
   // Only apply Detox plugin if E2E testing is enabled
+  // Otherwise we'd get this error on Android, when running the app outside of the e2e environment:
+  // `CLEARTEXT communication to [some host] not permitted by network security policy
+  // See also https://github.com/expo/config-plugins/tree/main/packages/detox#yarn-e2eandroid-failed
   if (process.env.EXPO_PUBLIC_MOBILE_STACK_E2E === 'true') {
     return withPlugins(config, ['@config-plugins/detox'])
   }
