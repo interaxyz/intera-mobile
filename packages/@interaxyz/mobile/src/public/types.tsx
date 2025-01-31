b0e49905ac26faeb3215978a5b476e9fa938fa04
jh2oman <jacobrwaterman@gmail.com>
2025-01-31 12:47:55 -0800

    feat(tabHeaderLogo): allow the logo that appears in the top left of the tab navigator to be configurable

--- packages/@interaxyz/mobile/src/public/types.tsx
+++ packages/@interaxyz/mobile/src/public/types.tsx
@@ -1,4 +1,4 @@
-import type { ImageSourcePropType } from 'react-native'
+import type { ImageSourcePropType } from 'react-native';
 
 // Type for tab configuration
 export interface TabScreenConfig {
@@ -103,6 +103,7 @@ export interface PublicAppConfig<tabScreenConfigs extends TabScreenConfig[] = Ta
         welcomeLogo?: React.ComponentType<any>
         welcomeBackgroundImage?: typeof require
         onboardingSuccessImage?: ImageSourcePropType
+        tabHeaderLogo?: React.ComponentType<any>
       }
     }
   }
